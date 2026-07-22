68cacc232196bb60ee11342f1c184cd298d9728f
eanglesartist <ccpanel.ag@gmail.com>
2026-07-22 04:58:53 -0500

    Update projects.py

--- backend/routers/projects.py
+++ backend/routers/projects.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Depends, HTTPException
+from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
 from .. import crud, schemas
 from ..database import get_db
